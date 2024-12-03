d3c056c7e13f753479ed157873fe1a41a5bce98e
rinaprince <marinacheca11@gmail.com>
2024-12-03 14:51:25 +0100

    Creant dades falses 5

--- src/DataFixtures/ArxiuFixtures.php
+++ src/DataFixtures/ArxiuFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Arxiu;
+use App\Entity\Obra;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ArxiuFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        //5 archius
+        for ($i = 0; $i < 5; $i++) {
+            $arxiu = new Arxiu();
+            $arxiu->setArxiuPdf($faker->filePath);
+            $arxiu->setArxiuPortada($faker->imageUrl(640, 480, 'business'));
+
+            $obra = $this->getReference('obra_' . $i);
+            $arxiu->addNumObra($obra);
+
+            $manager->persist($arxiu);
+        }
 
         $manager->flush();
     }
