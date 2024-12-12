41e01df9f84d35a3dc26416173c03528f2c3fa31
rinaprince <marinacheca11@gmail.com>
2024-12-12 19:28:51 +0100

    Fent formulari dinàmic 3

--- assets/vue/controllers/Autopubli_component.vue
+++ assets/vue/controllers/Autopubli_component.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 // Reactive state to track the selected option
 const selectedType = ref('');
@@ -7,47 +7,59 @@ const selectedType = ref('');
 
 <template>
   <div>
-    <!-- Radio buttons for selecting type -->
-    <div>
-      <label>
-        <input type="radio" name="type" value="digital" v-model="selectedType" /> Digital
-      </label>
-      <label>
-        <input type="radio" name="type" value="fisico" v-model="selectedType" /> Físico
-      </label>
+    <!-- Elegir -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+          <div class="form-check pe-5">
+            <input class="form-check-input" type="radio" name="type" value="digital" id="digital" v-model="selectedType" />
+            <label class="form-check-label" for="digital">
+              Digital
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="type" value="fisico" id="fisico" v-model="selectedType" />
+            <label class="form-check-label" for="fisico">
+              Físico
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <!-- Dynamic form based on selected type -->
+    <!-- Opcions -->
+    <div class="container">
+      <div class="row">
     <div v-if="selectedType === 'digital'">
-      <h3>Formulario para Digital</h3>
       <form>
-        <div>
-          <label for="digital-link">Enlace:</label>
-          <input id="digital-link" type="url" placeholder="Introduce el enlace" />
-        </div>
-        <div>
-          <label for="digital-format">Formato:</label>
-          <select id="digital-format">
+        <div class="col-6">
+          <select class="form-select" id="digital-format">
+            <option selected>Elige un formato</option>
             <option value="pdf">PDF</option>
             <option value="epub">EPUB</option>
             <option value="mobi">MOBI</option>
           </select>
         </div>
+        <div class="col-6">
+          <label for="digital-link">Portada:</label>
+          <input id="digital-link" type="url" placeholder="Sube tu portada"/>
+        </div>
       </form>
     </div>
 
     <div v-else-if="selectedType === 'fisico'">
-      <h3>Formulario para Físico</h3>
       <form>
         <div>
           <label for="physical-weight">Peso:</label>
-          <input id="physical-weight" type="number" placeholder="Introduce el peso en gramos" />
+          <input id="physical-weight" type="number" placeholder="Introduce el peso en gramos"/>
         </div>
         <div>
           <label for="physical-dimensions">Dimensiones:</label>
-          <input id="physical-dimensions" type="text" placeholder="Ej: 20x30x5 cm" />
+          <input id="physical-dimensions" type="text" placeholder="Ej: 20x30x5 cm"/>
         </div>
       </form>
+    </div>
+  </div>
     </div>
   </div>
 </template>
